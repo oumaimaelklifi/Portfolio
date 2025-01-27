ae1439f3c44894b4bceb0ab79c6afb4e66f91c70
oumaima <elkhlifi.oumaima.etu.uae.ac.ma>
2025-01-27 16:02:30 +0000

    contact

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Contact
 
 def Home_Page(request):
-    # Chargez la page HTML (sans appel récursif)
-    return render(request, 'index.html')
+    
+    if request== 'POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        message=request.POST.get('Message')
+
+         
+        if name and email and message :
+            contact=Contact(name=name , email=email , message=message)
+            contact.save()
+            context={'Success' :True}
+        else : 
+            context=context = {'error': 'Tous les champs sont obligatoires.'}
+        return render (request, 'index.html' ,context)
+ 
+
+    return render(request, 'index.html')  
